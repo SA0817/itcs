7c491f7365ed001d25e976427d78ae061243750f
Jonas <jonassanagustin17@gmail.com>
2024-12-16 15:05:07 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,15 @@
+name = input("Input your name: ")
+isStudent = input("Are you a current student of DLL (yes/no): ")
+
+if isStudent.lower() == "yes":
+    yearLevel = input("What year are you currently enrolled? \nF - Freshmen \nJ - Sophamore\nJ - Junior\nR - Senior \nPlease input your answer here: ")
+    if yearLevel.lower() == "f":
+        print(f"Hi {name}, Freshmen, Welcome to DLL")
+    elif yearLevel.lower() == "s":
+        print(f"Hi {name}, Sophamore, Welcome to DLL")
+    elif yearLevel.lower() == "j":
+        print(f"Hi {name}, Junior, Welcome to DLL")
+    elif yearLevel.lower() == "r":
+        print(f"Hi {name}, Senior, Welcome to DLL")
+else:
+    print("Thank you for using the program")
