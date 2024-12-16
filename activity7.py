c199246961275add548c03e10ae4606f40af430c
Jonas <jonassanagustin17@gmail.com>
2024-12-16 15:03:08 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,10 @@
+gold = 0
+miner = input ("Hi, what is your name? ")
+
+isGold = input ("Is the mineral gold? ") #yes
+
+if isGold == "yes"  :
+             gold += 1
+             print(f"Hi {miner}, your total number of gold is {gold}")
+else:
+             print(f"Hi {miner}, your total number of gold is {gold}")
