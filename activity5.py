65cf8f37edba42dc796c91f2bebcd1fcaf7ba7f5
Jonas <jonassanagustin17@gmail.com>
2024-12-16 15:01:29 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,6 @@
+#FARENHEIT TO CELSIUS
+
+print("FARENHEIT TO CELSIUS CONVERTER PROGRAN")
+farenheit = eval(input("Enter the temperature in Farenhiet: "))
+celcius = ((farenheit - 32) * 5)/9
+print (f"{farenheit}, degrees Farenheit converter to celsius is {round(celcius, 2)} degrees")
